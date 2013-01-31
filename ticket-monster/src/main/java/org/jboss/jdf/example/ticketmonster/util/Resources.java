e4f27e6a4ebd240ed93ce94d33c6c67883c9a6ca
shikanga <alexander@shikanga.co.uk>
2013-01-31 20:00:18 +0000

    - removed faces context

--- ticket-monster/src/main/java/org/jboss/jdf/example/ticketmonster/util/Resources.java
+++ ticket-monster/src/main/java/org/jboss/jdf/example/ticketmonster/util/Resources.java
@@ -2,10 +2,8 @@ package org.jboss.jdf.example.ticketmonster.util;
 
 import java.util.logging.Logger;
 
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -22,21 +20,26 @@ import javax.persistence.PersistenceContext;
  * </pre>
  */
 public class Resources {
-   // use @SuppressWarnings to tell IDE to ignore warnings about field not being referenced directly
+
+    /**
+     * Alias the persistence context
+     */
+    // use @SuppressWarnings to tell IDE to ignore warnings about field not being referenced directly
    @SuppressWarnings("unused")
    @Produces
    @PersistenceContext
    private EntityManager em;
    
+   /**
+    * Provider injectable loggers based around Java Util Logging.
+    * @param injectionPoint
+    * @return
+    */
    @Produces
    public Logger produceLog(InjectionPoint injectionPoint) {
       return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
    }
+ 
    
-   @Produces
-   @RequestScoped
-   public FacesContext produceFacesContext() {
-      return FacesContext.getCurrentInstance();
-   }
    
 }
